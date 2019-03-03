f026d9f2562709e09c4fb974df18e83b7ca358d2
LuisAngelGonzalezA <luigon42@gmail.com>
2019-03-03 12:54:01 -0600

    Agregar

--- TTI/Detener_Servicios/servicio.c
+++ TTI/Detener_Servicios/servicio.c
@@ -5,10 +5,26 @@ int main()
 {
 	
 	
+	/*
+	 * Recordatorio el servicio esta en 
+	 * /etc/system/system
+	 * 
+	 * 
+	 * Y por lo tanto con el sgueinte codigo podemos saber en que estado
+	 * se encuentra el servicio 
+	 * 
+	 * 0	=	Esta activo
+	 * 0	> 	Esta desactivado a lo cual, nos indica que esta desactivcado
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * */
+	 
 	
-	
-	int i=system("systemctl status pwm_calculado.service");
-	printf("El valor del servicivio es =%d",i);
+	int i=system("sudo systemctl is-active pwm_calculado.service");
+	printf("\nEl valor del servicivio es =%d\n",i);
 	return 0;
 	
 }
