2f370e0f1b58b1b0f31834b23f8c570410538671
LuisAngelGonzalezA <vivi_luis15@hotmail.com>
2019-03-21 02:13:54 -0600

    Agregar

---
+++ TTI/ensamblador/tt.X/build/default/production/USART PIC Raspberry.o.d
@@ -0,0 +1,3 @@
+build/default/production/USART\ PIC\ Raspberry.o:  \
+	C:/Users/luis/Desktop/Respaldo/tt.X/USART\ PIC\ Raspberry.asm  \
+
---
+++ TTI/ensamblador/ultima version/tt.X/build/default/production/USART PIC Raspberry.o.d
@@ -0,0 +1,3 @@
+build/default/production/USART\ PIC\ Raspberry.o:  \
+	C:/Users/luis/Desktop/Respaldo/ultima\ version/tt.X/USART\ PIC\ Raspberry.asm  \
+
