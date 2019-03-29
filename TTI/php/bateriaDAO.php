e4f31925ffa835c85adb2710e0eed9feca504e17
LuisAngelGonzalezA <luigon42@gmail.com>
2019-03-28 19:11:34 -0600

    Agregar

--- TTI/php/bateriaDAO.php
+++ TTI/php/bateriaDAO.php
@@ -9,11 +9,11 @@ $voltajeMin="2.1";//$_POST["voltajeMin"]
 $numeroCeldas="1";//$_POST["numeroCeldas"]
 $temperatura="10";//$_POST["temperatura"]
 $memoria="1";//$_POST["memoria"]
-*/
+
 $sql = "INSERT INTO bateria(id_mecanismo,nombre,voltaje_max, voltaje_min,corriente,nu_celdas,temperatura_max,hasMemoria)
  values(".$operacion.",'".$nombre."',".$voltajeMax.",".$voltajeMin.",0.0,".$numeroCeldas.",".$temperatura.",".$memoria.")";
- 
-if ($conn->query($sql) === TRUE) {
+*/ 
+if ($conn->query($operacion) === TRUE) {
     echo "New record created successfully.";
 } else {
     echo "Error updating record: $sql" . $conn->error;
