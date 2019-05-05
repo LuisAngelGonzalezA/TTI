f370eb5a69295e52abafe1be48cc7e118ab06411
LuisAngelGonzalezA <vivi_luis15@hotmail.com>
2019-05-05 17:22:05 -0500

    agregar

--- TTI/servo/hilo_servo/logica_movimiento_servo/movimiento_servo_de_servo.c
+++ TTI/servo/hilo_servo/logica_movimiento_servo/movimiento_servo_de_servo.c
@@ -43,6 +43,8 @@ void * movimiento_x(void *arg);
 void * movimiento_y(void *arg);
 
 
+void demonio()
+
 double voltaje_mayor_y,voltaje_mayor_x;
 int grados_dados=10;
 int grado_x,grados_y;
@@ -53,11 +55,12 @@ int tiempo_espera=4;
 
 int main()
 {
+	demonio(); 
 	
 	wiringPiSetupGpio();
 
-	pinMode(18,PWM_OUTPUT);
 	pinMode(13,PWM_OUTPUT);
+	pinMode(19,PWM_OUTPUT);
 	pwmSetMode(PWM_MODE_MS);
 	pwmSetClock(divisor);
 	pwmSetRange(range);
@@ -66,7 +69,7 @@ int main()
 	pthread_create(&tids[0],NULL,movimiento_x,NULL);
 	pthread_create(&tids[1],NULL,movimiento_y,NULL);
 
-	char *archivo="/home/pi/Desktop/archivo.txt";
+	char *archivo="/home/pi/TTI/TTI/servo/hilo_servo/logica_movimiento_servo/archivo.txt";
 	int archivo_exixte=existe(archivo);
 
 	
@@ -78,14 +81,15 @@ int main()
 		int posicion_servo_x=0.0,posicion_servo_y=0.0;
 		if(fichero>0){//Si el fichero se abre mal devuelve NULL
 	    
-			printf("	File Open\n");
+			//printf("	File Open\n");
+			syslog(LOG_INFO,"	File Open\n");
 			int i=0;
 			char lectura[100]={};
 			
 		
 			while(!feof(fichero)){//Esperamos el fin del fichero
 				//Leemos el fichero y lo printamos
-				printf("-->%s", fgets(lectura, 99, fichero));
+				//printf("-->%s", fgets(lectura, 99, fichero));
 				if(i==0)
 				{
 					//sscanf(lectura,"%f",&voltaje_ingresado);
@@ -105,37 +109,25 @@ int main()
 			{
 				posicion_servo_x=0.0;
 				posicion_servo_y=0.0;
-				printf("\n\nError faltan datos\n");
+				//printf("\n\nError faltan datos\n");
 				
 			}
 
 	}else{
 	    posicion_servo_x=0.0;
 		posicion_servo_y=0.0;
-	    printf("File not Open\n");
+	    //printf("File not Open\n");
 	    //syslog(LOG_INFO,"File not Open");
 	}
 	
-	printf("\nValores obtenidos del archvio son %d\n",posicion_servo_x);
-	printf("\nValores obtenidos del archvio son %d\n",posicion_servo_y);
+	//printf("\nValores obtenidos del archvio son %d\n",posicion_servo_x);
+	//printf("\nValores obtenidos del archvio son %d\n",posicion_servo_y);
 	grado_x=posicion_servo_x;
 	grados_y=posicion_servo_y;
 	sleep(1);
 	recalcular();
 	}
-	else if(archivo_exixte==-1)
-	{
-			/*No existe se ejecuta el comando para que se inicie los valores de default al posicionamiento 
-			 * del panel solar como un default al comienzo del día.*/
-		
-	}
-	else
-	{
-		/*Error con el archivo no se pudo leer debemos de atender este siniestro para poder seguir con la 
-		 * ejecución del programa para el posicionamiento adecuado y así efectuar el movimiento del panel 
-		 * para que simule el comportamiento del panel solar.*/	 
-		 
-	}
+	
 	
 	
 
@@ -174,12 +166,13 @@ double mysql_voltaje()
 	con=mysql_init(NULL);
 	if(!mysql_real_connect(con,server,user,pass,database,0,NULL,0))
 	{
-		fprintf(stderr, "%s\n", mysql_error(con));
+		//fprintf(stderr, "%s\n", mysql_error(con));
+		exit(1);
 	}
 
 	if(mysql_query(con,"select *,now()from sensadoP where hora between (now() -INTERVAL 10 SECOND) and (now()) order by hora desc limit 1"))
 	{
-		fprintf(stderr, "%s\n", mysql_error(con));
+		//fprintf(stderr, "%s\n", mysql_error(con));
 		exit(1);
 	}
 	res=mysql_use_result(con);
@@ -187,7 +180,7 @@ double mysql_voltaje()
 	//printf("La base de datos son :\n");
 	while((row= mysql_fetch_row(res)) !=NULL)
 		{
-			printf("%s\n",row[3]);
+			//printf("%s\n",row[3]);
 			 dato=atof(row[3]);
 			
 		}
@@ -201,7 +194,8 @@ double mysql_voltaje()
 
 void recalcular_y()
 {
-	printf("\nCalculando parte de y\n");
+	//printf("\nCalculando parte de y\n");
+	syslog(LOG_INFO,"	File Open\n");
 	double voltaje_referencia=mysql_voltaje(),voltaje_positivo_max,voltaje_negativo_max;
 	int posicion_temporal_y_positiva=grados_y,posicion_temporal_y_negativa=grados_y;
 	int bandera1=0,bandera2=0,bandera3=0,bandera4=0;
@@ -226,7 +220,7 @@ void recalcular_y()
 	{
 		voltaje_temporal=mysql_voltaje();
 		sleep(tiempo_espera);
-		printf("\nValore de comparación   %f :: %f\n",voltaje_temporal,voltaje_referencia);
+		//printf("\nValore de comparación   %f :: %f\n",voltaje_temporal,voltaje_referencia);
 		
 		if(voltaje_temporal>voltaje_referencia )
 		{	
@@ -235,7 +229,7 @@ void recalcular_y()
 			if(posicion_temporal_y_positiva>=180)
 			{
 				voltaje_referencia=voltaje_temporal;
-				printf("\nSe llegó al limite del panel en y : 180\n");
+				//printf("\nSe llegó al limite del panel en y : 180\n");
 				posicion_temporal_y_positiva=180;
 				grados_y=posicion_temporal_y_positiva;
 				bandera3=1;
@@ -244,7 +238,7 @@ void recalcular_y()
 			{
 				bandera4=1;
 				voltaje_referencia=voltaje_temporal;
-				printf("\nValores censados  positivos %d  <-> %d\n",grado_x,posicion_temporal_y_positiva);
+				//printf("\nValores censados  positivos %d  <-> %d\n",grado_x,posicion_temporal_y_positiva);
 
 				posicion_temporal_y_positiva=posicion_temporal_y_positiva+grados_dados;
 				if(posicion_temporal_y_positiva>=180)
@@ -288,13 +282,13 @@ void recalcular_y()
 			{
 				posicion_temporal_y_positiva=posicion_temporal_y_positiva-grados_dados;
 			}
-			printf("\n-->Fin--- El voltaje máximo obtenido es =%f con las posiciones x:%d <=> y:%d\n",voltaje_positivo_max,grado_x,posicion_temporal_y_positiva );
+			//printf("\n-->Fin--- El voltaje máximo obtenido es =%f con las posiciones x:%d <=> y:%d\n",voltaje_positivo_max,grado_x,posicion_temporal_y_positiva );
 			break;
 			
 		}	
 	}//while de positivo
 	bandera1=0;bandera2=0;bandera3=0;bandera4=0;
-	printf("\nCensando parte negativa\n");
+	//printf("\nCensando parte negativa\n");
 	grados_y=posicion_temporal_y_negativa;
 	sleep(tiempo_espera);
 	voltaje_referencia=mysql_voltaje();
@@ -320,7 +314,7 @@ void recalcular_y()
 	{
 		voltaje_temporal=mysql_voltaje();
 		sleep(1);
-		printf("\nValore de comparación   %f :: %f\n",voltaje_temporal,voltaje_referencia);
+		//printf("\nValore de comparación   %f :: %f\n",voltaje_temporal,voltaje_referencia);
 		
 		if(voltaje_temporal>voltaje_referencia )
 		{	
@@ -329,7 +323,7 @@ void recalcular_y()
 			if(posicion_temporal_y_negativa<0)
 			{
 				voltaje_referencia=voltaje_temporal;
-				printf("\nSe llegó al limite del panel en y : 0\n");
+				//printf("\nSe llegó al limite del panel en y : 0\n");
 				posicion_temporal_y_negativa=0;
 				grados_y=posicion_temporal_y_negativa;
 				sleep(tiempo_espera);
@@ -339,7 +333,7 @@ void recalcular_y()
 			{
 				bandera4=1;
 				voltaje_referencia=voltaje_temporal;
-				printf("\nValores censados  positivos %d  <-> %d\n",grado_x,posicion_temporal_y_negativa);
+				//printf("\nValores censados  positivos %d  <-> %d\n",grado_x,posicion_temporal_y_negativa);
 
 				posicion_temporal_y_negativa=posicion_temporal_y_negativa-grados_dados;
 				if(posicion_temporal_y_negativa<0)
@@ -382,7 +376,8 @@ void recalcular_y()
 			{
 				posicion_temporal_y_negativa=posicion_temporal_y_negativa+grados_dados;
 			}*/
-			printf("\n-->fin negativo El voltaje máximo obtenido es =%f con las posiciones x:%d <=> y:%d\n",voltaje_negativo_max,grado_x,posicion_temporal_y_negativa );
+			//printf("\n-->fin negativo El voltaje máximo obtenido es =%f con las posiciones x:%d <=> y:%d\n",voltaje_negativo_max,grado_x,posicion_temporal_y_negativa );
+			syslog(LOG_INFO,"\n-->fin negativo El voltaje máximo obtenido es =%f con las posiciones x:%d <=> y:%d\n",voltaje_negativo_max,grado_x,posicion_temporal_y_negativa );
 			break;
 			
 		}	
@@ -404,8 +399,8 @@ void recalcular_y()
 	}
 
 	sleep(tiempo_espera);
-	printf("\n\n ----------------Los valores que se obtuvieron son x : %d    y : %d\n\n",grado_x,grados_y);
-	
+	//printf("\n\n ----------------Los valores que se obtuvieron son x : %d    y : %d\n\n",grado_x,grados_y);
+	syslog(LOG_INFO,"\n\n ----------------Los valores que se obtuvieron son x : %d    y : %d\n\n",grado_x,grados_y);
 	
 	
 }
@@ -413,7 +408,8 @@ void recalcular_y()
 
 void recalcular_x()
 {
-	printf("\nCalculando parte de x\n");
+	//printf("\nCalculando parte de x\n");
+	syslog(LOG_INFO,"\nCalculando parte de x\n");
 	double voltaje_referencia=mysql_voltaje(),voltaje_positivo_max,voltaje_negativo_max;
 	int posicion_temporal_x_positiva=grado_x,posicion_temporal_x_negativa=grado_x;
 	int bandera1=0,bandera2=0,bandera3=0,bandera4=0;
@@ -438,7 +434,7 @@ void recalcular_x()
 	{
 		voltaje_temporal=mysql_voltaje();
 		sleep(1);
-		printf("\nValore de comparación   %f :: %f\n",voltaje_temporal,voltaje_referencia);
+		//printf("\nValore de comparación   %f :: %f\n",voltaje_temporal,voltaje_referencia);
 		
 		if(voltaje_temporal>voltaje_referencia )
 		{	
@@ -447,7 +443,7 @@ void recalcular_x()
 			if(posicion_temporal_x_positiva>=180)
 			{
 				voltaje_referencia=voltaje_temporal;
-				printf("\nSe llegó al limite del panel en y : 180\n");
+				//printf("\nSe llegó al limite del panel en y : 180\n");
 				posicion_temporal_x_positiva=180;
 				grado_x=posicion_temporal_x_positiva;
 				bandera3=1;
@@ -457,7 +453,7 @@ void recalcular_x()
 			{
 				bandera4=1;
 				voltaje_referencia=voltaje_temporal;
-				printf("\nValores censados  positivos %d  <-> %d\n",posicion_temporal_x_positiva,grados_y);
+				//printf("\nValores censados  positivos %d  <-> %d\n",posicion_temporal_x_positiva,grados_y);
 
 				posicion_temporal_x_positiva=posicion_temporal_x_positiva+grados_dados;
 				if(posicion_temporal_x_positiva>=180)
@@ -500,14 +496,15 @@ void recalcular_x()
 			{
 				posicion_temporal_x_positiva=posicion_temporal_x_positiva-grados_dados;
 			}
-			printf("\n-->Fin El voltaje máximo obtenido es =%f con las posiciones x:%d <=> y:%d\n",voltaje_positivo_max,posicion_temporal_x_positiva,grados_y);
+			//printf("\n-->Fin El voltaje máximo obtenido es =%f con las posiciones x:%d <=> y:%d\n",voltaje_positivo_max,posicion_temporal_x_positiva,grados_y);
+			syslog(LOG_INFO,"\n-->Fin El voltaje máximo obtenido es =%f con las posiciones x:%d <=> y:%d\n",voltaje_positivo_max,posicion_temporal_x_positiva,grados_y);
 			break;
 			
 		}	
 	}//while de positivo
 	bandera1=0;bandera2=0;bandera3=0;bandera4=0;
-	printf("\nCensando parte negativa\n");
-	
+	//printf("\nCensando parte negativa\n");
+	syslog(LOG_INFO,"\nCensando parte negativa\n");
 	grado_x=posicion_temporal_x_negativa;
 	sleep(tiempo_espera);
 	voltaje_referencia=mysql_voltaje();
@@ -533,7 +530,7 @@ void recalcular_x()
 	{
 		voltaje_temporal=mysql_voltaje();
 		sleep(1);
-		printf("\nValore de comparación   %f :: %f\n",voltaje_temporal,voltaje_referencia);
+		//printf("\nValore de comparación   %f :: %f\n",voltaje_temporal,voltaje_referencia);
 		
 		if(voltaje_temporal>voltaje_referencia )
 		{	
@@ -542,7 +539,7 @@ void recalcular_x()
 			if(posicion_temporal_x_negativa<0)
 			{
 				voltaje_referencia=voltaje_temporal;
-				printf("\nSe llegó al limite del panel en y : 0\n");
+				//printf("\nSe llegó al limite del panel en y : 0\n");
 				posicion_temporal_x_negativa=0;
 				grado_x=posicion_temporal_x_negativa;
 				bandera3=1;
@@ -553,7 +550,7 @@ void recalcular_x()
 			{
 				bandera4=1;
 				voltaje_referencia=voltaje_temporal;
-				printf("\nValores censados  positivos %d  <-> %d\n",posicion_temporal_x_negativa,grados_y);
+				//printf("\nValores censados  positivos %d  <-> %d\n",posicion_temporal_x_negativa,grados_y);
 
 				posicion_temporal_x_negativa=posicion_temporal_x_negativa-grados_dados;
 				if(posicion_temporal_x_negativa<0)
@@ -597,7 +594,8 @@ void recalcular_x()
 			{
 				posicion_temporal_x_negativa=posicion_temporal_x_negativa+grados_dados;
 			}*/
-			printf("\n-->Fin El voltaje máximo obtenido es =%f con las posiciones x:%d <=> y:%d\n",voltaje_negativo_max,posicion_temporal_x_negativa,grados_y);
+			//printf("\n-->Fin El voltaje máximo obtenido es =%f con las posiciones x:%d <=> y:%d\n",voltaje_negativo_max,posicion_temporal_x_negativa,grados_y);
+			syslog(LOG_INFO,"\n-->Fin El voltaje máximo obtenido es =%f con las posiciones x:%d <=> y:%d\n",voltaje_negativo_max,posicion_temporal_x_negativa,grados_y);
 			break;
 			
 		}	
@@ -618,7 +616,8 @@ void recalcular_x()
 		grado_x=posicion_temporal_x_positiva;
 	}
 	sleep(tiempo_espera);
-	printf("\n\n ----------------Los valores que se obtuvieron son x : %d    y : %d\n\n",grado_x,grados_y);
+	//printf("\n\n ----------------Los valores que se obtuvieron son x : %d    y : %d\n\n",grado_x,grados_y);
+	syslog(LOG_INFO,"\n\n ----------------Los valores que se obtuvieron son x : %d    y : %d\n\n",grado_x,grados_y);
 	
 }
 void recalcular()
@@ -638,7 +637,7 @@ void recalcular()
 
 void guardar_datos(int x,int y)
 {
-	printf("\nGuardando\n");
+	//printf("\nGuardando\n");
 	FILE *fichero;
 	char lectura[100];
 	fichero=fopen("/home/pi/Desktop/archivo.txt","wt"); //Abrimos el fichero para solo lectura
@@ -662,7 +661,7 @@ void * movimiento_x(void *arg)
   while(1)
   {
     int posicion=posicion_panel(grado_x);
-    pwmWrite(18,posicion);
+    pwmWrite(13,posicion);
     usleep(50);
   }
 }
@@ -672,8 +671,104 @@ void * movimiento_y(void *arg)
   {
     int posicion=posicion_panel(grados_y);
     //syslog(LOG_INFO,"\tRecalcular la ecuacion: \tgrados=%d  --  pwm %d\n",posicion,grados_y);
-    pwmWrite(13,posicion);
+    pwmWrite(19,posicion);
     usleep(50);
   }
 }
  
+
+void demonio()
+{
+
+FILE *apArch;
+
+    pid_t pid = 0;
+    pid_t sid = 0;
+// Se crea el proceso hijo
+    pid = fork();
+    if( pid == -1 )
+    {
+		perror("Error al crear el primer proceso hijo\n");
+		exit(EXIT_FAILURE);
+    }
+/*
+ * Se termina Proceso padre.
+ * Al finalizar el proceso padre el proceso hijo es adoptado por init. 
+ * El resultado es que la shell piensa que el comando terminó con éxito, 
+ * permitiendo que el proceso hijo se ejecute de manera independiente en segundo plano.
+ */
+    if( pid )
+    {
+		printf("Se termina proceso padre, PID del proceso hijo %d \n", pid);
+		exit(0);
+    }
+/* Se restablece el modo de archivo
+ * Todos los procesos tiene una máscara que indica que permisos no deben establecerse al crear nuevos archivos. 
+ * Así cuando se utilizan llamadas al sistema como open() los permisos especificados se comparan con esta máscara, 
+ * desactivando de manera efectiva los que en ella se indiquen.
+ * La máscara —denominada umask()— es heredada de padres a hijos por los procesos, por lo que su valor por defecto 
+ * será el mismo que el que tenía configurada la shell que lanzó el demonio. Esto significa que el demonio no sabe 
+ * que permisos acabarán tenido los archivos que intente crear. Para evitarlo simplemente podemos autorizar todos 
+ * los permisos 
+ */
+    umask(0);
+/*
+ * se inicia una nueva sesion
+ * Cada proceso es miembro de un grupo y estos a su vez se reúnen en sesiones. En cada una de estas hay un proceso 
+ * que hace las veces de líder, de tal forma que si muere todos los procesos de la sesión reciben una señal SIGHUP.
+ * La idea es que el líder muere cuando se quiere dar la sesión por terminada, por lo que mediante SIGHUP se 
+ * notifica al resto de procesos esta circunstancia para que puedan terminar ordenadamente.
+ * Obviamente no estamos interesados en que el demonio termine cuando la sesión desde la que fue creado finalice, 
+ * por lo que necesitamos crear nuestra propia sesión de la que dicho demonio será el líder.
+ */
+    sid = setsid();
+    if( sid < 0 )
+    {
+		perror("Error al iniciar sesion");
+		exit(EXIT_FAILURE);
+    }
+// Se realiza un segundo fork para separarnos completamente de la sesion del padre
+    pid = fork( );
+    if( pid == -1 )
+    {
+		perror("Error al crear el segundo proceso hijo\n");
+		exit(EXIT_FAILURE);
+    }
+    if( pid )
+    {
+		printf("PID del segundo proceso hijo %d \n", pid);
+		apArch = fopen("/home/pi/UARTDATOS.pid", "w");
+		fprintf(apArch, "%d", pid);
+		fclose(apArch);
+
+		exit(0);
+    }
+/* 
+ * Se cambia el directorio actual por root.
+ * Hasta el momento el directorio de trabajo del proceso es el mismo que el de la shell en el momento en el
+ * que se ejecutó el comando. Este podría estar dentro de un punto de montaje cualquiera del sistema, por lo
+ * que no tenemos garantías de que vaya a seguir estando disponible durante la ejecución del proceso.
+ * Por eso es probable que prefiramos cambiar el directorio de trabajo al directorio raíz, ya que podemos
+ * estar seguros de que siempre existirá
+ */
+    chdir("/");
+/*
+ * Se cierran los flujos de entrada y salida: stdin, stdout, stderr
+ * Puesto que un demonio se ejecuta en segundo plano no debe estar conectado a ninguna terminal. 
+ * Sin embargo esto plantea la cuestión de cómo indicar condiciones de error, advertencias u otro 
+ * tipo de sucesos del programa. Algunos demonios almacenan estos mensajes en archivos específicos 
+ * o en su propia base de datos de sucesos. Sin embargo en muchos sistemas existe un servicio especifico 
+ * para registrar estos eventos. En lo sistemas basados en UNIX este servicio lo ofrece el demonio Syslog, 
+ * al que otros procesos pueden enviar mensajes a través de la función syslog()
+ */
+    close( STDIN_FILENO  );
+    close( STDOUT_FILENO );
+    close( STDERR_FILENO );
+// Se abre un archivo log en modo de escritura.
+    openlog( "UARTDATOS", LOG_NDELAY | LOG_PID, LOG_LOCAL0 );
+
+    
+    closelog( );
+
+
+}
